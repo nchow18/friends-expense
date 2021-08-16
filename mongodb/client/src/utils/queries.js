5f817f0096bdacba1cfc776425cf312eabe3c1d4
Nathan Chow <emailme@nathanchow.ca>
2021-08-16 01:13:27 -0400

    update

--- mongodb/client/src/utils/queries.js
+++ mongodb/client/src/utils/queries.js
@@ -12,6 +12,8 @@ query user($user_id: ID!) {
     email
     phone
     events {
+      _id
+      event_id
       event_name
     }
   }
@@ -25,15 +27,93 @@ query {userMe {
   last_name
   email
   phone
-  cart {
-    product_id
-    quantity
+  events {
+    _id
+    event_name
+    event_id
   }
-  address{
-    street_name
-    street_number
-  }
-  pastOrders
 }
+`;
+
+export const EVENT = gql`
+query event($event_id: ID!) {
+  event(event_id:$event_id) {
+    _id
+    event_id
+    title
+    notes
+    activities {
+      _id
+      name
+      date
+      time
+      cost
+      address
+      website
+      map
+      event_id
+    }
+    acitivity_ideas {
+      _id
+      name
+      date
+      time
+      cost
+      address
+      website
+      map
+      votes
+      event_id
+    }
+    itinerary {
+      _id
+      name
+      date
+      time
+      event_id
+    }
+    expense {
+      _id
+      item
+      cost
+      user
+      paid
+      event_id
+    }
+    meal {
+      _id
+      date
+      meal_type
+      ingredients
+      time
+      prepared_by
+      event_id
+    }
+    meal_ideas {
+      _id
+      votes
+      meal_name
+      meal_type
+      date
+      time
+      event_id
+    }
+    groceries {
+      _id
+      item
+      added_by
+      purchased
+      purchased_by
+      event_id
+    }
+    total_cost
+    split_cost {
+      _id
+      name
+      contributions
+      owing
+      event_id
+    }
+  }
 }
 `;
